88a073efbe4631934599310b49eac4ecdf244936
pip-delorean <christgx5@gmail.com>
2020-11-21 21:41:46 -0600

    Make performance improvements to clustering.

--- app/Http/Controllers/ClusteringController.php
+++ app/Http/Controllers/ClusteringController.php
@@ -13,30 +13,36 @@ use Phpml\Clustering\KMeans;
 
 class ClusteringController extends Controller
 {
-  public function dbscan() {
+  public function dbscan(Request $request) {
+    $ignore_unknown_weapon = $request->input('ignore_unknown_weapon') ?? true;
     set_time_limit(10000);
+
+    if ($ignore_unknown_weapon === "false") {
+      $ignore_unknown_weapon = false;
+    } else {
+      $ignore_unknown_weapon = true;
+    }
+
     $offenses = Offense::select('OFFENSE_ID', 'OFFENSE_TYPE_ID')->with('type', 'weapon', 'weapon.type')->get();
     $unknown_weapon = Weapon::with('type')->find(19);
-    $data = $offenses->map(function ($offense) use ($unknown_weapon) {
+    $data = $offenses->map(function ($offense) use ($unknown_weapon, $ignore_unknown_weapon) {
       if ($offense->weapon == null) {
         $offense->weapon = $unknown_weapon;
       }
+      if ($ignore_unknown_weapon && $offense->weapon->type->WEAPON_ID == 19) {
+        return null;
+      }
       return [$offense->type->OFFENSE_TYPE_ID, $offense->weapon->type->WEAPON_ID];
     });
-    $samples = $data->toArray();
+
+    $samples = array_filter($data->toArray(), static function($value){return $value !== null;} );
 
     $dbscan = new DBSCAN($epsilon = 1, $minSamples = 50);
     $clusters = collect($dbscan->cluster($samples));
 
-    $ignore_unknown_weapon = true;
-
     foreach ($clusters as $cluster_id => $cluster) {
       $offense_id = $cluster[0][0];
       $weapon_id = $cluster[0][1];
-      if ($weapon_id == 19 && $ignore_unknown_weapon) {
-        unset($clusters[$cluster_id]);
-        continue;
-      }
       $offense_name = OffenseType::find($offense_id)->OFFENSE_NAME;
       $weapon_name = WeaponType::find($weapon_id)->WEAPON_NAME;
       $cluster_name = $offense_name." + ".$weapon_name;
@@ -48,30 +54,37 @@ class ClusteringController extends Controller
     return view('offense_type_vs_weapon_type', compact("data", "ignore_unknown_weapon"));
   }
 
-  public function kmeans() {
+  public function kmeans(Request $request) {
+    $ignore_unknown_weapon = $request->input('ignore_unknown_weapon') ?? true;
     set_time_limit(10000);
+
+    if ($ignore_unknown_weapon === "false") {
+      $ignore_unknown_weapon = false;
+    } else {
+      $ignore_unknown_weapon = true;
+    }
+
     $offenses = Offense::select('OFFENSE_ID', 'OFFENSE_TYPE_ID')->with('type', 'weapon', 'weapon.type')->get();
     $unknown_weapon = Weapon::with('type')->find(19);
-    $data = $offenses->map(function ($offense) use ($unknown_weapon) {
+    $data = $offenses->map(function ($offense) use ($unknown_weapon, $ignore_unknown_weapon) {
       if ($offense->weapon == null) {
         $offense->weapon = $unknown_weapon;
       }
+      if ($ignore_unknown_weapon && $offense->weapon->type->WEAPON_ID == 19) {
+        return null;
+      }
       return [$offense->type->OFFENSE_TYPE_ID, $offense->weapon->type->WEAPON_ID];
     });
-    $samples = $data->toArray();
+
+    $samples = array_filter($data->toArray(), static function($value){return $value !== null;} );
 
     $kmeans = new KMeans(17);
     $clusters = collect($kmeans->cluster($samples));
 
-    $ignore_unknown_weapon = true;
-
     foreach ($clusters as $cluster_id => $cluster) {
-      $offense_id = $cluster[0][0];
-      $weapon_id = $cluster[0][1];
-      if ($weapon_id == 19 && $ignore_unknown_weapon) {
-        unset($clusters[$cluster_id]);
-        continue;
-      }
+      $first_item = array_values($cluster)[0];
+      $offense_id = $first_item[0];
+      $weapon_id = $first_item[1];
       $offense_name = OffenseType::find($offense_id)->OFFENSE_NAME;
       $weapon_name = WeaponType::find($weapon_id)->WEAPON_NAME;
       $cluster_name = $offense_name." + ".$weapon_name;
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,10 @@
     <div class="flex flex-justify-center flex-align-end">
       <ul>
         <li><a href="/offense_vs_location">Offense VS Location</a></li>
-        <li><a href="/offense_type_vs_weapon_type_dbscan">Offense Type VS Weapon Type (DBSCAN), </a><b>Will take ~15 minutes (~46,000 data points)</b></li>
-        <li><a href="/offense_type_vs_weapon_type_kmeans">Offense Type VS Weapon Type (KMeans), </a><b>Will take ~15 minutes (~46,000 data points)</b></li>
+        <li><a href="/offense_type_vs_weapon_type_dbscan">Offense Type VS Weapon Type (DBSCAN), </a></li>
+        <li><a href="/offense_type_vs_weapon_type_dbscan?ignore_unknown_weapon=false">Offense Type VS Weapon Type (DBSCAN, Including Unknown Weapons), </a><b>Will take ~15 minutes (~46,000 data points)</b></li>
+        <li><a href="/offense_type_vs_weapon_type_kmeans">Offense Type VS Weapon Type (KMeans)</a></li>
+        <li><a href="/offense_type_vs_weapon_type_kmeans?ignore_unknown_weapon=false">Offense Type VS Weapon Type (KMeans, Including Unknown Weapons)</a></li>
         <li><a href="/offender_ages">Offenders By Age</a></li>
       </ul>
     </div>
