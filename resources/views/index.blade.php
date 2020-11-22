95f9825414ca4b1a7b49bdd4774814549eea1f4b
pip-delorean <christgx5@gmail.com>
2020-11-21 21:44:38 -0600

    Update index.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,12 @@
 @section('content')
   <div id="root">
     <div class="flex flex-justify-center flex-align-end">
+      <h1>Nebraska 2018 FBI Crime Statistics</h1>
+      <h2>Choose a visualization/analysis (Most should run in a minute or less):</h2>
       <ul>
         <li><a href="/offense_vs_location">Offense VS Location</a></li>
         <li><a href="/offense_type_vs_weapon_type_dbscan">Offense Type VS Weapon Type (DBSCAN), </a></li>
-        <li><a href="/offense_type_vs_weapon_type_dbscan?ignore_unknown_weapon=false">Offense Type VS Weapon Type (DBSCAN, Including Unknown Weapons), </a><b>Will take ~15 minutes (~46,000 data points)</b></li>
+        <li><a href="/offense_type_vs_weapon_type_dbscan?ignore_unknown_weapon=false">Offense Type VS Weapon Type (DBSCAN, Including Unknown Weapons)</a><b>, Will take ~15 minutes (~46,000 data points)</b></li>
         <li><a href="/offense_type_vs_weapon_type_kmeans">Offense Type VS Weapon Type (KMeans)</a></li>
         <li><a href="/offense_type_vs_weapon_type_kmeans?ignore_unknown_weapon=false">Offense Type VS Weapon Type (KMeans, Including Unknown Weapons)</a></li>
         <li><a href="/offender_ages">Offenders By Age</a></li>
